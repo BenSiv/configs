43a07f91de4555454351225389bda7a5adfe5698
BenSiv <bensiv92@gmail.com>
2023-05-14 12:57:07 +0300

    updating julia startup configs

--- startup.jl
+++ startup.jl
@@ -1,7 +1,14 @@
-using JuliaSyntax
-JuliaSyntax.enable_in_core!(true)
-
-using Pkg
-if isfile("Project.toml") && isfile("Manifest.toml")
-    Pkg.activate(".")
+atreplinit() do repl
+    @eval begin
+        import JuliaSyntax
+        JuliaSyntax.enable_in_core!(true)
+    end
 end
+
+using OhMyREPL
+colorscheme!("JuliaDefault")
+
+# using Pkg
+# if isfile("Project.toml") && isfile("Manifest.toml")
+#     Pkg.activate(".")
+# end
