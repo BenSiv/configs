207923406491da9269738322d19395a15deb3b97
bensiv <bensiv92@gmail.com>
2025-08-07 13:37:16 -0700

    replacing docker with podman and adding aliases

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,7 +1,12 @@
 # my pretty print
-alias readdir='ls --format=single-column --almost-all --group-directories-first --color=auto'
-alias rd=readdir
+alias readdir="ls --format=single-column --almost-all --group-directories-first --color=auto"
+alias rd="lua /root/lua-automations/readdir.lua"
+alias edit="lua /root/lua-automations/edit.lua"
+alias find="lua /root/lua-automations/find.lua"
+alias repo="lua /root/lua-automations/repo.lua"
+alias sqlite="sqlite3"
+alias python="python3"
+alias brex="/root/brain-ex/brex"
 
-alias sqlite='sqlite3'
-alias ted='gnome-text-editor'
-alias mde='docker exec -it min-dev-env bash'
+alias ted="gnome-text-editor"
+alias mde="podman exec -it min-dev-env bash"
--- min_dev_env/deploy.sh
+++ min_dev_env/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Stop and remove the existing container if it exists
-docker stop "min-dev-env"
-docker rm "min-dev-env"
+podman stop "min-dev-env"
+podman rm "min-dev-env"
 
 # Remove the existing image if it exists
-docker image rm "min-dev-env"
+podman image rm "min-dev-env"
 
-# Build the Docker image
-docker build \
-    --build-arg LUA_VERSION="5.4" \
+# Build the podman image
+podman build \
+    --build-arg LUA_VERSION="5.1" \
     --build-arg LUAROCKS_VERSION="3.10" \
     --tag "min-dev-env" .
 
-# Run the Docker container
-docker run \
+# Run the podman container
+podman run \
     --detach --tty --name "min-dev-env" \
     --hostname "min-dev-env" \
     "min-dev-env"
