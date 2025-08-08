709f38958b84f68cafbb07941217ca4bef918c1d
bensiv <bensiv92@gmail.com>
2025-08-08 12:21:19 -0700

    updating dev entrypoint

--- min_dev_env/entrypoint.sh
+++ min_dev_env/entrypoint.sh
@@ -2,12 +2,13 @@
 
 # Append the commands to ~/.bashrc
 cat /root/configs/bashrc.sh >> ~/.bashrc
+cp /root/configs/bash_aliases.sh >> ~/.bash_aliases
 source ~/.bashrc
 
 bash /root/lua-utils/install_requirements.sh
 
-cp /root/configs/micro /root/.config/
-cp /root/configs/gitconfig.toml /root/.gitconfig
+cp -r /root/configs/micro /root/.config/
+cp /root/git/configs/gitconfig.toml /root/.gitconfig
 
 # Run the specified command or default to starting a shell
 exec "${@:-bash}"
