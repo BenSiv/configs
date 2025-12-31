56b97fe6a341175458004708de295e6cddcebe42
bensiv <bensiv92@gmail.com>
2025-12-31 13:32:39 -0800

    Update ISO builder for CIDATA method: partition setup, meta-data, and fix recursive copy

--- ansible/iso-builder/build.sh
+++ ansible/iso-builder/build.sh
@@ -25,6 +25,9 @@ mkdir -p "$WORK_DIR"
 echo "Extracting ISO..."
 7z x "$UBUNTU_ISO" -o"$WORK_DIR" -y > /dev/null
 
+# Remove integrity check (since we are modifying content)
+rm -f "$WORK_DIR/md5sum.txt"
+
 # Prepare ncloud directory
 mkdir -p "$WORK_DIR/ncloud"
 cp user-data "$WORK_DIR/ncloud/user-data"
@@ -32,9 +35,8 @@ touch "$WORK_DIR/ncloud/meta-data" # Empty meta-data is fine
 
 # Inject Ansible configuration
 echo "Injecting Ansible..."
-cp -r ../ "$WORK_DIR/ansible"
-# Exclude the iso-builder itself to avoid recursion/bloat
-rm -rf "$WORK_DIR/ansible/iso-builder"
+mkdir -p "$WORK_DIR/ansible"
+cp -r ../roles ../site.yml ../inventory ../ansible.cfg ../install.sh "$WORK_DIR/ansible/"
 
 # Patch GRUB to enable Autoinstall
 echo "Patching GRUB..."
@@ -46,19 +48,21 @@ sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/ncloud/ ---|g' "$WORK_DIR/boot/
 echo "Building new ISO..."
 cd "$WORK_DIR"
 xorriso -as mkisofs -r \
-  -V 'ubuntu-cidata' \
+  -V 'Ubuntu-Server 24.04.3 LTS amd64' \
   -o "../$OUTPUT_ISO" \
-  --grub2-mbr "../$WORK_DIR/boot/grub/i386-pc/boot_hybrid.img" \
+  --grub2-mbr "--interval:local_fs:0s-15s:zero_mbrpt,zero_gpt:../$UBUNTU_ISO" \
+  --protective-msdos-label \
+  -partition_cyl_align off \
   -partition_offset 16 \
   --mbr-force-bootable \
-  -append_partition 2 28732ac11ff8d211eba50090273fc04f "../$WORK_DIR/boot/grub/efi.img" \
+  -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b "--interval:local_fs:6441216d-6451375d::../$UBUNTU_ISO" \
   -appended_part_as_gpt \
   -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
   -c '/boot.catalog' \
   -b '/boot/grub/i386-pc/eltorito.img' \
     -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
   -eltorito-alt-boot \
-  -e '--interval:appended_partition_2:all::' \
+  -e '--interval:appended_partition_2_start_1610304s_size_10160d:all::' \
     -no-emul-boot \
   . 
 
